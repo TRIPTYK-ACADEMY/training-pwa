67f01318ea0906ec5dccd8ea8bd2e3fa84d67880
remadex <s.cardon@outlook.com>
2023-04-13 15:54:03 +0200

    cache assets

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,10 @@
-self.addEventListener("install", event => {
-  console.log("Service worker installed");
-});
+const assets = ["/", "style.css", "app.js", "sw-register.js", 
+"https://fonts.googleapis.com/css2?family=Noto+Sans&family=Poppins:wght@700&display=swap", "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"];
 
-self.addEventListener("activate", event => {
-  console.log("Service worker activated");
+self.addEventListener("install", event => {
+  event.waitUntil( // If a file is heavy and take a long time before loading
+    caches.open("assets").then( cache => {
+        cache.addAll(assets);
+    })
+  )
 });
